12a7e4e4c9c83ba4c05a4fd183f495fd659771c5
Salro-72 <85388678+Salro-72@users.noreply.github.com>
2021-07-13 15:18:48 +0200

    Add files via upload

--- login.php
+++ login.php
@@ -2,98 +2,88 @@
 session_start();
 require 'configs/db.php';
 
-if(isset($_SESSION["userloggedin"]) && $_SESSION["userloggedin"] === TRUE){
-    header("location: user_connected/homepage.php");
-    exit;
-}
-
-$username = $password = "";
-$username_error = $password_error = "";
-
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $username = trim($_POST["username"]);
-    $password = trim($_POST["password"]);
-
-if(empty($username_error) && empty($password_error)){
-
+if(isset($_POST['connexion'])){
+    
+    //Retrieve the field values from our login form.
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+    
+    //Retrieve the user account information for the given username.
     $sql = "SELECT id, username, password FROM users WHERE username = :username";
-    if($stmt = $pdo->prepare($sql)){
-    $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
-    $param_username = trim($_POST["username"]);
-
-    if($stmt->execute()){
-        if($stmt->rowCount() == 1){
-            if($row = $stmt->fetch()){
-                $id = $row["id"];
-                $username = $row["username"];
-                $hashed_password = $row["password"];
-                if(password_verify($password, $hashed_password)){
-                    session_start();
-
-                    $_SESSION["userloggedin"] = TRUE;
-                    $_SESSION["id"] = $id;
-                    $_SESSION["username"] = $username;
-
-                    header("location: homepage.php");
-                } else{
-                    $password_error = "The password you entered was not valid.";
-                }
-            }
-            else{
-                $username_error = "No account found with that username or email address.â€‹â€‹â€‹â€‹â€‹";
-            }
+    $stmt = $pdo->prepare($sql);
+    
+    //Bind value.
+    $stmt->bindValue(':username', $username);
+    
+    //Execute.
+    $stmt->execute();
+    
+    //Fetch row.
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    //If $row is FALSE.
+    if($user === false){
+        //Could not find a user with that username!
+        //PS: You might want to handle this error in a more user-friendly manner!
+        die('Identifiant ou mot de passe invalide!');
+    } else{
+        //User account found. Check to see if the given password matches the
+        //password hash that we stored in our users table.
+        
+        //Compare the passwords.
+        $validPassword = password_verify($passwordAttempt, $user['password']);
+        
+        //If $validPassword is TRUE, the login has been successful.
+        if($validPassword){
+            
+            //Provide the user with a login session.
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['logged_in'] = time();
+            
+            header('Location: homepage.php');
+            exit;
+            
         } else{
-          $alert = "Something went wrong. Please Try Again.";
-          echo "<script type='text/javascript'>alert('$alert');</script>";
+            die('Incorrect username / password combination!');
         }
     }
-    }
-
-unset($pdo);
+    
 }
-}
+ 
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8" />
+    <meta charset="UTF-8" />
         <link rel="stylesheet" type="text/css" href="style_GBAF.css"/>
         <title>Extranet GBAF</title>
-    </head>
-
+    </head>    
         <body>
             <header>
                 <?php include("php/logo.php"); ?>
             </header>
-    
-            <div class="title">
-            <h2>Connectez-vous</h2>
+        <body>
+            <div class="title_box">
+                <h1>Connectez-vous à votre espace membre:</h1>
             </div>
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="option_box">
-                    <div class="label_class"><label>Identifiant</label></div>
-                    <div class="input_class"><input type="text" name="username" value="<?php echo $username; ?>" required></div>
-                        <span><?php echo $username_error; ?></span>
-                    <br>
-                    <div class="label_class"><label>Mot de passe</label></div>
-                    <div class="input_class"><input type="password" name="password" value="<?php echo $password; ?>" required></div>
-                        <span><?php echo $password_error; ?></span>
-                    <br>
-                    <div class="row">
-                        <div class="input_class">
-                            <input type="submit" value="Envoyer">
-                        </div>
-                    </div> 
-                    <br>
-                   
-                    <div class="new_password">
-                        <p><a href="forgotpassword.php">Mot de passe oublié?</a></p>
-                    </div>  
+            <form action="login.php" method="post" class="option_box">
+
+                <label for="username">Identifiant</label>
+                <input type="text" id="username" name="username" required><br>
+            
+                <label for="password">Mot de passe</label>
+                <input type="password" id="password" name="password" required><br>
+            
+                <input type="submit" name="connexion" value="Connexion">
+
+                <div class="new_password">
+                    <p><a href="forgotpassword.php">Mot de passe oublié?</a></p>
+                </div>  
                               
-                    <div class="new_inscription">
-                        <p><a href="signup.php">Première visite?</a></p>
-                    </div>   
-                </form>
+                <div class="new_inscription">
+                    <p><a href="signup.php">Première visite?</a></p>
+                </div> 
+            </form>
         </body>
     <?php include("php/footer.php"); ?>
 </html>
--- login_new.php
+++ login_new.php
@@ -1,4 +1,72 @@
 <?php
+session_start();
+require 'configs/db.php';
+
+if(isset($_POST['connexion'])){
+    
+
+    $username = !empty($_POST['username']) ? trim($_POST['username']) : null;
+    $passwordAttempt = !empty($_POST['password']) ? trim($_POST['password']) : null;
+ 
+    $sql = "SELECT id, username, password FROM users WHERE username = :username";
+    $stmt = $pdo->prepare($sql);
+    
+    $stmt->bindValue(':username', $username);
+    
+    $stmt->execute();
+    
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if($user === false){
+        die('Incorrect username / password combination!');
+    } else{
+        $validPassword = password_verify($passwordAttempt, $user['password']);
+        
+        if($validPassword){
+            
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['logged_in'] = time();
+            
+            header('Location: homepage.php');
+            exit;
+            
+        } else{
+            die('Incorrect username / password combination!');
+        }
+    }
+    
+}
+ 
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8" />
+        <link rel="stylesheet" type="text/css" href="style_GBAF.css"/>
+        <title>Extranet GBAF</title>
+    </head>    
+        <body>
+            <header>
+                <?php include("php/logo.php"); ?>
+            </header>
+        <body>
+        <div class="title">
+                <h1>Votre compte a bien été crée!</h1>
+            </div>
+                
+            <div class="title">
+                <h2>Connectez-vous à votre espace membre</h2>
+            </div> 
+            <form action="login.php" method="post" class="option_box">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username"><br>
+                <label for="password">Password</label>
+                <input type="text" id="password" name="password"><br>
+                <input type="submit" name="connexion" value="Connexion">
+            </form>
+        </body>
+    <?php include("php/footer.php"); ?>
+</html><?php
 
 session_start();
 require 'configs/db.php';
