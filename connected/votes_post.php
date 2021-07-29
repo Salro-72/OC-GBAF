9ab133ae46c7abaf504304b2e8f2e3860a15f39b
Salro-72 <85388678+Salro-72@users.noreply.github.com>
2021-07-29 12:11:46 +0200

    Add files via upload

--- connected/votes_post.php
+++ connected/votes_post.php
@@ -5,19 +5,19 @@ require '../configs/db.php';
 if (isset($_POST['vote'])){
     $vote = $_POST['vote'];
 
-if ($vote == $likes) {
+if ($vote === "J'aime") {
 // Insertion du vote à l'aide d'une requête préparée
 $req = $pdo->prepare('INSERT INTO votes (id_acteur, id_user, vote) VALUES(?, ?, 1)');
 $req->execute(array($_GET['billet'], $_SESSION['id_user'], $_POST['vote']));
 
-echo "Votre vote a été validé.";
+echo "Votre vote a été validé. (like)";
 }
 
-if ($vote == $dislikes) {
+if ($vote === "Je n'aime pas") {
     // Insertion du vote à l'aide d'une requête préparée
     $req = $pdo->prepare('INSERT INTO votes (id_acteur, id_user, vote) VALUES(?, ?, 0)');
     $req->execute(array($_GET['billet'], $_SESSION['id_user'], $_POST['vote']));
     
-    echo "Votre vote a été validé.";
+    echo "Votre vote a été validé. (dislike)";
     }
 }
