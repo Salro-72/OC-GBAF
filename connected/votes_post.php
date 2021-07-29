a59ef7f28bd65ebd2003d32ec4c8000e3ff33c9e
Salro-72 <85388678+Salro-72@users.noreply.github.com>
2021-07-29 11:54:25 +0200

    Add files via upload

--- connected/votes_post.php
+++ connected/votes_post.php
@@ -5,7 +5,7 @@ require '../configs/db.php';
 if (isset($_POST['vote'])){
     $vote = $_POST['vote'];
 
-if ($number == 1) {
+if ($vote == $likes) {
 // Insertion du vote à l'aide d'une requête préparée
 $req = $pdo->prepare('INSERT INTO votes (id_acteur, id_user, vote) VALUES(?, ?, 1)');
 $req->execute(array($_GET['billet'], $_SESSION['id_user'], $_POST['vote']));
@@ -13,12 +13,11 @@ $req->execute(array($_GET['billet'], $_SESSION['id_user'], $_POST['vote']));
 echo "Votre vote a été validé.";
 }
 
-elseif ($number == 0) {
-// Insertion du vote à l'aide d'une requête préparée
-$req = $pdo->prepare('UPDATE votes SET vote = :vote WHERE id_acteur = ? AND id_user = ? AND vote = 0');
-$req->execute(array($_GET['billet'], $_SESSION['id_user'], $_POST['vote']));
-$dislikes = $req->fetch(PDO::FETCH_ASSOC)['dislikes'];
-                
-echo "Votre vote a été validé.";
-}
+if ($vote == $dislikes) {
+    // Insertion du vote à l'aide d'une requête préparée
+    $req = $pdo->prepare('INSERT INTO votes (id_acteur, id_user, vote) VALUES(?, ?, 0)');
+    $req->execute(array($_GET['billet'], $_SESSION['id_user'], $_POST['vote']));
+    
+    echo "Votre vote a été validé.";
+    }
 }
