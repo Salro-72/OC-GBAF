6295805f92620549014795ddfb3f6b59fcae0e90
Salro-72 <85388678+Salro-72@users.noreply.github.com>
2021-07-29 07:48:41 +0200

    Add files via upload
    MIS À JOUR = limiter le nombre des commentaires (un commentaire par utilisateur / par article)

--- connected/commentaires_post.php
+++ connected/commentaires_post.php
@@ -2,11 +2,20 @@
 session_start();
 require '../configs/db.php';
 
-// Insertion du message à l'aide d'une requête préparée
-$req = $pdo->prepare('INSERT INTO commentaires (id_billet, id_user, commentaire, date_commentaire) VALUES(?, ?, ?, NOW())');
-$req->execute(array($_GET['billet'], $_POST['id_user'], $_POST['commentaire']));
 
- 
-// Redirection du visiteur vers la liste des commentaires
-header('Location: acteurs.php?billet=' . $_GET['billet']);
-?>
+$req = $pdo->prepare('SELECT * FROM comments WHERE id_acteur = ? AND id_user = ?');
+$req->execute(array($_GET['billet'], $_POST['id_user']));
+$number = $req->rowCount();
+
+if (!$number) {
+    // Insertion du message à l'aide d'une requête préparée
+    $req = $pdo->prepare('INSERT INTO comments (id_acteur, id_user, comment, date_comment) VALUES(?, ?, ?, NOW())');
+    $req->execute(array($_GET['billet'], $_POST['id_user'], $_POST['comment']));
+
+    // Redirection du visiteur vers la liste des commentaires
+    header('Location: acteurs.php?billet=' . $_GET['billet']);
+}
+
+else {
+   echo "Vous avez déjà commenté! <br> <a href='acteurs.php?billet=". $_GET['billet'] ."'> Retour arriere</a>";
+}
